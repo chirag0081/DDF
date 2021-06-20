e95215fc36f05ca76371b8ac15acdfbab73bbf5b
Chirag patel <chirag0081@hotmail.com>
2021-06-20 09:18:40 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace DDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePaths = Directory.GetFiles(@"C:\Users\chira\source\repos\DDF\ScanFiles");
+            foreach (var item in filePaths)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
